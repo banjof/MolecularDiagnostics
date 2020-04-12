b7b78b710cc6829a00f784fff4089ad91900ff03
Folarin Banjo <you@example.comcoolfob@yahoo.com>
2020-04-12 11:09:40 +0100

    Update 12/04/20

--- assets/js/labto-map.js
+++ assets/js/labto-map.js
@@ -8,14 +8,14 @@
     window.initMap = function() {
         var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
-        center: {lat: 59.325, lng: 18.070}
+        center: {lat: 6.5860, lng: 3.3459}
         });
 
         marker = new google.maps.Marker({
         map: map,
         draggable: true,
         animation: google.maps.Animation.DROP,
-        position: {lat: 59.327, lng: 18.067}
+        position: {lat: 6.5860, lng: 3.3459}
         });
         marker.addListener('click', toggleBounce);
     }
