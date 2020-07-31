cb87ba74626aa2b2db3c111caafe3ad74cdd00c3
banjof <coolfob@yahoo.com>
2020-07-31 13:22:34 +0100

    Not fixed

--- assets/php/form-process.php
+++ assets/php/form-process.php
@@ -10,9 +10,9 @@ use PHPMailer\PHPMailer\Exception;
 
 
 //Required phpmailer files
-require '../PHPMailer/src/Exception.php';
-require '../PHPMailer/src/PHPMailer.php';
-require '../PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
@@ -35,15 +35,15 @@ try {
     $mail->Port       = 25;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 
-//Gmail Server Settings for local host
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-$mail->isSMTP();                                            // Send using SMTP
-$mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-$mail->Username   = 'vartechsolutionsltd@gmail.com';                     // SMTP username
-$mail->Password   = '';                               // SMTP password
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-$mail->Port       = 587;       
+// //Gmail Server Settings for local host
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+// $mail->isSMTP();                                            // Send using SMTP
+// $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+// $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+// $mail->Username   = 'vartechsolutionsltd@gmail.com';                     // SMTP username
+// $mail->Password   = '';                               // SMTP password
+// $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+// $mail->Port       = 587;       
 
 // smtp_server=smtp.gmail.com
 // smtp_port=587
