0cd4c3394ffd57806d67bd73852d298cbcac4fd7
banjof <coolfob@yahoo.com>
2020-07-31 13:17:30 +0100

    Mail Fix

--- assets/php/form-process.php
+++ assets/php/form-process.php
@@ -16,7 +16,7 @@ require '../PHPMailer/src/SMTP.php';
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
-$name = $_POST[''];
+$name = $_POST['name'];
 $emailAddress = $_POST['email'];
 $subject = $_POST['msg_subject'];
 $phoneNumber = $_POST['phone_number'];
@@ -27,29 +27,49 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'localhost';                    // Set the SMTP server to send through
+    $mail->Host       = 'moleculardiagnostics.com.ng';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'info@moleculardiagnostics.com.ng';                     // SMTP username
     $mail->Password   = 'Fo3~ts01';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 25;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
+
+//Gmail Server Settings for local host
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+$mail->isSMTP();                                            // Send using SMTP
+$mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+$mail->Username   = 'vartechsolutionsltd@gmail.com';                     // SMTP username
+$mail->Password   = '';                               // SMTP password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+$mail->Port       = 587;       
+
+// smtp_server=smtp.gmail.com
+// smtp_port=587
+// error_logfile=error.log
+// debug_logfile=debug.log
+// auth_username=YourGmailId@gmail.com
+// auth_password=Your-Gmail-Password
+// force_sender=YourGmailId@gmail.com(optional)
+
+
     //Recipients
     $mail->setFrom($emailAddress, $name );
     $mail->addAddress('info@moleculardiagnostics.com.ng', 'Enquiry');     // Add a recipient
-    $mail->addAddress('ellen@example.com');               // Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+    // $mail->addAddress('ellen@example.com');               // Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
 
     // Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = $subject;
-    $mail->Body    = $body;
+    $mail->Body    = $message;
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
